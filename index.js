ac2aaad1ab98bad9e999d88b1ea31d9e69e4d5eb
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-02-19 23:14:59 +0600

    arithmetic operator

---
+++ index.js
@@ -0,0 +1,110 @@
+// var - let - const start
+
+/*
+// var
+var myName = `Mehedi Hasan Himel`
+console.log(myName);
+
+myName = `Rajib Rahman`
+
+console.log(myName);
+*/
+
+// let
+/*
+
+let myName = `Mehedi Hasan Himel`;
+console.log(myName);
+
+myName = `Rajib`;
+console.log(myName);
+
+*/
+
+// const
+
+/*
+const myName = `mehedi hasan himel`
+myName = `rajib`
+console.log(myName);
+*/
+// var - let - const start
+
+/*
+// Data Type Start
+
+// String 
+const person = `Rajib`
+console.log(typeof person)
+
+// number
+const no = 50
+console.log(typeof no)
+
+// boolean
+const isTrue = true 
+console.log(typeof isTrue)
+
+// null
+const test = null
+console.log(typeof test);
+
+// undefined
+let himel;
+console.log(typeof himel);
+
+// Data Type End
+*/
+
+/* 
+// Arithmetic Operator Start
+
+// Addition
+let x = 10
+let y = 20
+let z = x + y
+console.log(z);
+
+// Subtraction
+let a = 30;
+let b = 20;
+let c = a - b;
+console.log(c);
+
+// Multiplication 
+let a = 30;
+let b = 20;
+let c = a * b;
+console.log(c);
+
+// Exponentiation 
+let a = 3;
+let b = 2;
+let c = a ** b;
+console.log(c);
+
+// Division
+let a = 30;
+let b = 20;
+let c = a / b;
+console.log(c);
+
+// Modulus
+let a = 30;
+let b = 20;
+let c = a % b;
+console.log(c);
+
+// Increment
+let a = 30;
+a++ //a++ = a + 1;
+console.log(a);
+
+// decrement
+let a = 30;
+a-- //a-- = a - 1;
+console.log(a);
+
+*/
+
+// Arithmetic Operator End
