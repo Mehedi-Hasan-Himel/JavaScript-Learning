9d6625fea57a1d53623996b82630b45ce44f7fdf
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-02-19 23:28:21 +0600

    Assignment Operator

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ console.log(myName);
 myName = `Rajib Rahman`
 
 console.log(myName);
-*/
+
 
 // let
-/*
+
 
 let myName = `Mehedi Hasan Himel`;
 console.log(myName);
@@ -19,18 +19,18 @@ console.log(myName);
 myName = `Rajib`;
 console.log(myName);
 
-*/
+
 
 // const
 
-/*
+
 const myName = `mehedi hasan himel`
 myName = `rajib`
 console.log(myName);
-*/
+
 // var - let - const start
 
-/*
+
 // Data Type Start
 
 // String 
@@ -54,57 +54,94 @@ let himel;
 console.log(typeof himel);
 
 // Data Type End
-*/
-
-/* 
+ 
 // Arithmetic Operator Start
 
-// Addition
-let x = 10
-let y = 20
-let z = x + y
-console.log(z);
+   // Addition
+   let x = 10
+   let y = 20
+   let z = x + y
+   console.log(z);
 
-// Subtraction
-let a = 30;
-let b = 20;
-let c = a - b;
-console.log(c);
+   // Subtraction
+   let a = 30;
+   let b = 20;
+   let c = a - b;
+   console.log(c);
 
-// Multiplication 
-let a = 30;
-let b = 20;
-let c = a * b;
-console.log(c);
+   // Multiplication 
+   let a = 30;
+   let b = 20;
+   let c = a * b;
+   console.log(c);
 
-// Exponentiation 
-let a = 3;
-let b = 2;
-let c = a ** b;
-console.log(c);
+   // Exponentiation 
+   let a = 3;
+   let b = 2;
+   let c = a ** b;
+   console.log(c);
 
-// Division
-let a = 30;
-let b = 20;
-let c = a / b;
-console.log(c);
+   // Division
+   let a = 30;
+   let b = 20;
+   let c = a / b;
+   console.log(c);
 
-// Modulus
-let a = 30;
-let b = 20;
-let c = a % b;
-console.log(c);
+   // Modulus
+   let a = 30;
+   let b = 20;
+   let c = a % b;
+   console.log(c);
 
-// Increment
-let a = 30;
-a++ //a++ = a + 1;
-console.log(a);
+   // Increment
+   let a = 30;
+   a++ //a++ = a + 1;
+   console.log(a);
 
-// decrement
-let a = 30;
-a-- //a-- = a - 1;
-console.log(a);
-
-*/
+   // decrement
+   let a = 30;
+   a-- //a-- = a - 1;
+   console.log(a);
 
 // Arithmetic Operator End
+
+
+
+// Assignment Operator Start
+   // += 
+   let x = 10;
+   x += 5; //x = x + 5;
+   console.log(x);
+   
+   // -= 
+   let x = 20;
+   x -= 5; //x = x - 5;
+   console.log(x);
+   
+
+   // *= 
+   let x = 20;
+   x *= 5; //x = x * 5;
+   console.log(x);
+
+
+   // /= 
+   let x = 20;
+   x /= 5; //x = x / 5;
+   console.log(x);
+
+
+   // %= 
+   let x = 20;
+   x %= 3; //x = x % 3;
+   console.log(x);
+
+
+   // **= 
+   let x = 2;
+   x **= 3; //x = x * 3;
+   console.log(x);
+   
+*/
+
+// Assignment Operator End
