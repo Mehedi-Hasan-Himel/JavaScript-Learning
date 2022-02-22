cac9234c20b7d82a3197f070d87900ede5fd3331
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-02-22 21:29:35 +0600

    if else

--- index.js
+++ index.js
@@ -224,3 +224,34 @@ console.log(x < 10 || y > 12); //true
 // Conditional Ternary operator
 // syntax variableName = (condition) ? true : false
 // Example: let voteAble = (Age < 18) ? "Too Young" : "Old Enough"
+
+// Conditional Statement Start
+/*
+let time = 4;
+if (time >= 5 && time < 12) {
+  console.log(`Good morning.`);
+} else if (time >= 12 && time <= 14) {
+  console.log(`Good noon.`);
+} else if (time >= 15 && time <= 19) {
+  console.log(`Good evening.`);
+} else {
+  console.log(`Good night`);
+}
+
+
+let mark = 32;
+if (mark >= 80) {
+  console.log(`A+`);
+} else if (mark <= 79 && mark > 69) {
+  console.log(`A`);
+} else if (mark <= 69 && mark > 59) {
+  console.log(`A-`);
+} else if (mark <= 59 && mark > 49) {
+  console.log(`B`);
+} else if (mark <= 49 && mark >= 33) {
+  console.log(`D`);
+} else {
+   console.log(`Fail`);
+}
+*/
+// Conditional Statement End
