3a8f0a75a91fe5573020a5e1062a09a46755bc33
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-04-04 02:09:45 +0600

    object

---
+++ object.js
@@ -0,0 +1,21 @@
+let student = {
+  id: 115,
+  name: `Mehedi Hasan Himel`,
+  marks: 81,
+  phone: 01969038472,
+};
+
+let macBook = {
+  color: `Silver`,
+  name: `MacBook Air 2017`,
+  screenSize: 13,
+  ram: `8 GB`,
+};
+
+let book = {
+  title: `Start with why`,
+  author: `Simon Sinek`,
+  price: 200,
+  pages: 246,
+  publisher: penguin,
+};
