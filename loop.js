9c41a02dd45dcd7e7576cc1c1dfbd383d573d0dc
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-04-03 19:07:07 +0600

    loop

--- loop.js
+++ loop.js
@@ -11,6 +11,18 @@ while (number <= 10) {
    console.log(number);
    number++
 }
-*/ 
+
+let number = 2;
+while (number <= 20) {
+  console.log(number);
+  number += 2;
+}
+
+let number = 1;
+while (number <= 20) {
+  console.log(number);
+  number += 2;
+}
+*/
 
 // While Loop End
