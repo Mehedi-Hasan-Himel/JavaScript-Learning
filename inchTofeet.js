0388f70b9a67febbd7ad79e7e5021a193d5ba964
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-04-05 05:20:27 +0600

    even

---
+++ inchTofeet.js
@@ -0,0 +1,39 @@
+/*
+let inches = 132;
+let feet = inches / 12;
+console.log(inches , `inches =`,feet,`feet`);
+
+let dadiInches = 144;
+let dadiFeet = dadiInches / 12;
+console.log(dadiInches, `inches =`, dadiFeet, `feet`);
+
+
+function inchToFeet(inches) {
+   let feet = inches / 12;
+   return feet;
+}
+
+let inches = 132;
+let feet = inchToFeet(inches)
+console.log(inches, `inches =`, feet, `feet`);
+
+
+let dadiInches = 144;
+let dadiFeet = inchToFeet(dadiInches);
+console.log(dadiInches, `inches =`, dadiFeet, `feet`);
+
+
+function isEven(number) {
+  if (number % 2 == 0) {
+    return true;
+  }
+  return false;
+}
+
+const myNumber = 1641;
+const isMyNumberEven = isEven(myNumber);
+
+console.log(isMyNumberEven);
+
+*/
+
