ea335cb2caf085ee0aabca279d11fd592a0de775
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-04-04 12:17:25 +0600

    code

--- object.js
+++ object.js
@@ -1,8 +1,9 @@
+/*
 let student = {
   id: 115,
   name: `Mehedi Hasan Himel`,
   marks: 81,
-  phone: 01969038472,
+  phone: +8801969038472,
 };
 
 let macBook = {
@@ -17,5 +18,16 @@ let book = {
   author: `Simon Sinek`,
   price: 200,
   pages: 246,
-  publisher: penguin,
+  publisher: `penguin`,
 };
+*/
+
+let computer = {
+  price: 80000,
+  storage: `128 GB`,
+   processor: `intel i5`,
+   color: `Silver`,
+};
+let storageProperty = `storage`;
+computer[storageProperty]=`512 GB `
+console.log(computer);
