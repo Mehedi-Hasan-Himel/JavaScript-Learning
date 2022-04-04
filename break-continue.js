b8a9318e3bd788ce074d3473a8fb39b0086417ec
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-04-04 12:54:40 +0600

    break

---
+++ break-continue.js
@@ -0,0 +1,26 @@
+/*
+let i = 0;
+while (i < 10) {
+  console.log(i);
+  if (i == 5) {
+    break;
+  }
+  i++;
+}
+
+for (let index = 0; index <= 20; index++) {
+  console.log(index);
+  if (index == 5) {
+    break;
+  }
+}
+*/
+
+let numbers = [44, 33, 22, 11, 55, 66, 7, 88, 77, 8, 99, 89];
+for (let i = 0; i < numbers.length; i++) {
+  let number = numbers[i];
+  console.log(number);
+  if (number > 60) {
+    break;
+  }
+}
