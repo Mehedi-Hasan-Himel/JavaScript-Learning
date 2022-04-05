f511c5bdc3b2afcd25246c9ed5cd4a97bc2778c5
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-04-05 17:39:29 +0600

    swap

---
+++ swap.js
@@ -0,0 +1,14 @@
+let first = 5;
+let second = 7;
+console.log(first, second);
+
+// first approach.
+
+// let temp = first;
+// first = second;
+// second = temp;
+// console.log(first, second);
+
+// destructuring 
+[first, second] = [second, first];
+console.log(first, second);
