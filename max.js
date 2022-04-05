40cce4c92527a3f4024ff02ff00c7e1a897c61c5
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-04-05 18:00:18 +0600

    Math.max()

--- max.js
+++ max.js
@@ -1,6 +1,9 @@
 // তিনটি সংখ্যার মধ্যে বড় সংখ্যা নির্ণয় করো ।
 
-const business = 450;
+/*
+
+// option-01-if else condition.
+const business = 250;
 const minister = 350;
 const army = 600;
 
@@ -11,4 +14,15 @@ if (business > minister && business > army) {
 } else {
   console.log(`Army is bigger.`);
 }
+*/
 
+/*
+
+// option-02- Math.max();
+const business = 250;
+const minister = 350;
+const army = 600;
+
+let max = Math.max(business, minister, army);
+console.log(`Largest is`, max);
+*/
