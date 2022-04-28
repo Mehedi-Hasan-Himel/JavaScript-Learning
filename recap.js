c247325f35f450ce47cb1b0a551005c289c9c0cd
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-04-28 22:35:23 +0600

    code

---
+++ recap.js
@@ -0,0 +1,9 @@
+function largestElement(numbers) {
+  let max = numbers[0];
+  for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    if (element > max) {
+      max = element;
+    }
+  }
+}
