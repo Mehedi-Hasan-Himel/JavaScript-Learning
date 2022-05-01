fffd317e86b1699c6e5fca30312793f92a95a03d
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-05-01 08:47:15 +0600

    check-list.js

---
+++ .vscode/check-list.js
@@ -0,0 +1,23 @@
+// 1. Comment In Js.
+
+// 2. Write 3 variables. (Number , String , Boolean)
+
+// 3. 2 variables using . (let, Const)
+
+// 4. Simple Math operation(+, -, *, /, %)
+
+// 5. Comparison (<, >, ===, <=, >=, =!)
+
+// 6. Two conditoion. Case 1: Fulfill both condition. Case 2: Fulfill at list one condition. 
+
+// 7. if-else.
+
+// 8. While loop to display 7 to 19 all numbers. only display odd numbers between 7 to 19 .
+
+// 9. Declear an array. Number an element. 4th position update or change. add or remove elements. Check whether specific value exists in the array. 
+
+// 10. use any for loop to display every elements of an array 
+
+// 11. Your have an array of numbers. Display only the bigger then 80. 
+
+// 12. Write a function that takes 3 numbers and returns  the multiplications of the 3 numbers 
