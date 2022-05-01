4f523fc0944e5b0cd44bc3910f4a2e0c07c7e379
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-05-01 11:38:42 +0600

    3

--- check-list.js
+++ check-list.js
@@ -12,18 +12,30 @@ let isTrue = true;
 
 // 3. 2 variables using . (let, Const)
 
+let name = `Himel`;
+myName = `Mehedi Hasan Himel`;
+console.log(myName);
+/*
+A variable which is declared by 'let' , can change it's value multiple time. 
+*/
+
+const PI = 3.1416;
+console.log(PI);
+/*
+A variable which is declared by 'const' , can not change it's value after declare time. 
+*/ 
 
 // 4. Simple Math operation(+, -, *, /, %)
 
 // 5. Comparison (<, >, ===, <=, >=, =!)
 
-// 6. Two conditoion. Case 1: Fulfill both condition. Case 2: Fulfill at list one condition. 
+// 6. Two condition. Case 1: Fulfill both condition. Case 2: Fulfill at list one condition. 
 
 // 7. if-else.
 
 // 8. While loop to display 7 to 19 all numbers. only display odd numbers between 7 to 19 .
 
-// 9. Declear an array. Number an element. 4th position update or change. add or remove elements. Check whether specific value exists in the array. 
+// 9. Declare an array. Number an element. 4th position update or change. add or remove elements. Check whether specific value exists in the array. 
 
 // 10. use any for loop to display every elements of an array 
 
