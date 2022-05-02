7b5e54ca315ad0e5a93a0c83bd650a49cc1a4e8d
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-05-02 20:56:01 +0600

    only display odd numbers between  to 19

--- check-list.js
+++ check-list.js
@@ -6,26 +6,31 @@ Multiline comment.
 */
 
 // 2. Write 3 variables. (Number , String , Boolean)
+/*
 let number = 5;
 let sentence = `This is a pen.`;
 let isTrue = true;
+*/
 
 // 3. 2 variables using . (let, Const)
-
+/*
 let name = `Himel`;
 myName = `Mehedi Hasan Himel`;
 console.log(myName);
+*/
 /*
 A variable which is declared by 'let' , can change it's value multiple time. 
 */
-
+/*
 const PI = 3.1416;
 console.log(PI);
+*/
 /*
 A variable which is declared by 'const' , can not change it's value after declare time. 
 */
 
 // 4. Simple Math operation(+, -, *, /, %)
+/* 
 let number1 = 5;
 let number2 = 7;
 
@@ -43,10 +48,9 @@ console.log(div);
 
 let mud = number2 % number1;
 console.log(mud);
-
-
+*/
 // 5. Comparison (<, >, ===, <=, >=, !=)
-
+/*
 let a = 6;
 let b = 8;
 
@@ -67,9 +71,9 @@ console.log(gaterThanOrEqual);
 
 let notEqual = a != b;
 console.log(notEqual);
-
-// 6. Two condition. Case 1: Fulfill both condition. Case 2: Fulfill at list one condition. 
-
+*/
+// 6. Two condition. Case 1: Fulfill both condition. Case 2: Fulfill at list one condition.
+/* 
 let num1 = 2, num2 = 13 , num3 = 5;
 
 if (num1 > 5 && num1 < 10) {
@@ -77,26 +81,41 @@ if (num1 > 5 && num1 < 10) {
    
 } else if(num1 >= 5 || num1 <= 10){
       console.log(`The number is between 5 to 10`);
+} 
+*/
+
+// 7. While loop to display 7 to 19 all numbers. only display odd numbers between 7 to 19 . only display even numbers between 7 to 19 .
+
+// While loop to display 7 to 19 all numbers.
+/*
+let number7 = 7;
+while (number7 <= 19) {
+  console.log(number7);
+  number7++;
 }
+*/
+// only display odd numbers between 7 to 19 .
+/*
+let number71 = 7;
+while (number71 <= 19) {
+  let increase = number71++;
+  if (increase % 2 === 0) {
+    console.log(increase);
+  }
+}
+let number71 = 7;
+while (number71 <= 19) {
+  let increase = number71++;
+  if (increase % 2 ==! 0) {
+    console.log(increase);
+  }
+}
+*/ 
 
+// 8. Declare an array. Number an element. 4th position update or change. add or remove elements. Check whether specific value exists in the array.
 
-// a = 5 , b = 7 c = ? 
-// case-1: && 
-// case-2: ||  
+// 9. use any for loop to display every elements of an array
 
+// 10. Your have an array of numbers. Display only the bigger then 80.
 
-// 7. if-else.
-
-// 8. While loop to display 7 to 19 all numbers. only display odd numbers between 7 to 19 .
-
-// 9. Declare an array. Number an element. 4th position update or change. add or remove elements. Check whether specific value exists in the array. 
-
-// 10. use any for loop to display every elements of an array 
-
-// 11. Your have an array of numbers. Display only the bigger then 80. 
-
-// 12. Write a function that takes 3 numbers and returns  the multiplications of the 3 numbers 
-
-
-
-
+// 11. Write a function that takes 3 numbers and returns  the multiplications of the 3 numbers
