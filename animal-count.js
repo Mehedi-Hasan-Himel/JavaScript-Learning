09dd9fe9b6ef5e464981f248029deed7ab848187
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-05-23 10:49:23 +0600

    code

---
+++ animal-count.js
@@ -0,0 +1,42 @@
+// Village to Forest 0 - 10 kilo = per kilo 10 animals.
+
+// Village to Forest 10 - 20 kilo = per kilo 50 animals.
+
+// Village to Forest 20+  kilo = per kilo 100 animals.
+
+function animalCount(miles) {
+  const animalDensityFirst10Miles = 10;
+  const animalDensitySecond10Miles = 50;
+  if (miles <= 10) {
+    const Count = miles * animalDensityFirst10Miles;
+    return Count;
+  } else if (miles <= 20) {
+    const firstDenseAnimals = 10 * animalDensityFirst10Miles;
+
+    const restMiles = miles - 10;
+
+    const secondDenseAnimals = restMiles * animalDensitySecond10Miles;
+
+    const totalAnimals = firstDenseAnimals + secondDenseAnimals;
+
+    return totalAnimals;
+  } else {
+    const firstDenseAnimals = 10 * animalDensityFirst10Miles;
+
+    const secondDenseAnimals = 10 * animalDensitySecond10Miles;
+
+    const restMiles = miles - 20;
+
+    const animalDensityRestMiles = restMiles * 100;
+
+    const RestDenseAnimals = restMiles * animalDensityRestMiles;
+
+    const totalAnimals =
+      firstDenseAnimals + secondDenseAnimals + RestDenseAnimals;
+    return totalAnimals;
+  }
+}
+
+const animals = animalCount(25);
+
+console.log(animals);
