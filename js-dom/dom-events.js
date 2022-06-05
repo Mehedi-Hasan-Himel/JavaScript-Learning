8dcce9fc5697e6f816490b4b593371e2b4da8d96
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-06-06 01:30:26 +0600

    blue

--- js-dom/dom-events.js
+++ js-dom/dom-events.js
@@ -5,3 +5,12 @@ function makeRed() {
 function makeYellow() {
   document.body.style.backgroundColor = `Yellow`;
 }
+
+const blueButton = document.getElementById("make-blue-button");
+
+blueButton.onclick = makeBlue;
+
+function makeBlue() {
+  document.body.style.backgroundColor = `Blue`;
+}
+
