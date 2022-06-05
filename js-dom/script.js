bddb02bf6af83807806cc59d815561711fcb64e5
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-06-06 01:29:08 +0600

    red

--- js-dom/script.js
+++ js-dom/script.js
@@ -19,7 +19,6 @@ const grandParent = children.closest(".todo-list").previousElementSibling;
 
 console.log(grandParent);
 
-*/
 
 // creating an element
 
@@ -36,3 +35,24 @@ const container = document.querySelector(".todo-list");
 const h2Element = container.querySelector("h2");
 
 container.insertBefore(divElement, h2Element);
+
+const divElement = document.createElement("div");
+divElement.className = `red`;
+divElement.setAttribute(`id`, `red`);
+divElement.setAttribute(`title`, `Red Div`);
+
+const container = document.querySelector(`.todo-list`);
+// const h2Element = container.querySelector(`h2`);
+// container.insertBefore(divElement,h2Element);
+
+// const a = container.appendChild(divElement);
+const b = container.append(divElement);
+
+console.log(b);
+*/
+
+const formElement = document.querySelector("form");
+
+formElement.addEventListener("keydown", (event) => {
+  console.log(event.target.value);
+});
