536497fd0487fe08bf60f5e879f3bb26b6206db7
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-06-10 11:32:27 +0600

    events

---
+++ js-dom/comment.js
@@ -0,0 +1,27 @@
+
+/*
+// step-1: add button event handler
+
+document
+  .getElementById("submit-comment")
+  .addEventListener("click", function () {
+    // Step-2: Get user comment
+    const commentBox = document.getElementById("new-comment");
+    console.log(commentBox.value);
+  });
+
+//   step-3: create comment paragraph
+const newComment = document.createElement("p");
+newComment.innerText = commentBox.value;
+
+// Step-4: append the comment
+const commentContainer = document.getElementById("comment-container");
+
+commentContainer.appendChild(newComment)
+
+// step-5: Clean the comment box
+
+commentBox.value = " ";
+
+*/
+
--- js-dom/event-common.js
+++ js-dom/event-common.js
@@ -5,10 +5,16 @@ function handleJustFunctionClick() {
 }
 
 // Direct.
-document.getElementById("handle-event").addEventListener('click',function(){
-   const p = document.getElementById('clicking-method');
-    p.innerText = 'set by direct add event listener'
+document.getElementById("handle-event").addEventListener("click", function () {
+  const p = document.getElementById("clicking-method");
+  p.innerText = "set by direct add event listener";
 });
 
+// direct
+document.getElementById("update-name").addEventListener("click", function () {
+  const nameField = document.getElementById("name-input-field");
 
-
+  const p = document.getElementById("clicking-method");
+  p.innerText = nameField.value;
+  nameField.value = " ";
+});
