ab8e2b9e53c6991c4a7112467fc15653b8257109
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-06-20 02:47:10 +0600

    code

--- js-dom/bap-er-hotel/banking.js
+++ js-dom/bap-er-hotel/banking.js
@@ -31,3 +31,36 @@ document
     // clear the deposit input
     depositInput.value = " ";
   });
+
+// handle withdraw event handler
+document
+  .getElementById("withdraw-button")
+  .addEventListener("click", function () {
+    const withdrawInput = document.getElementById("withdraw-input");
+    const withdrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(withdrawAmountText);
+    console.log(newWithdrawAmount);
+
+    // Set withdraw total
+    const withdrawTotal = document.getElementById("withdraw-total");
+
+    const previousWithdrawText = withdrawTotal.innerText;
+
+    const previousWithdrawTotal = parseFloat(previousWithdrawText);
+
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+
+    withdrawTotal.innerText = newWithdrawTotal;
+
+    // update balance
+    const balanceTotal = document.getElementById("balance-total");
+    const previousBalanceText = balanceTotal.innerText;
+    previousBalanceTotal = parseFloat(previousBalanceText);
+
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+
+    balanceTotal.innerText = newBalanceTotal;
+
+    // clear withdraw input
+    withdrawInput.value = " ";
+  });
