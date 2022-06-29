d794399fc2691cf4b363c9df9ae2f5bec6142a72
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-06-30 02:19:10 +0600

    toLowerCase toUpperCase

---
+++ js-string-array/case-convert.js
@@ -0,0 +1,8 @@
+const anthem = "Amar Sonar Bangla, Ami Tomay Vhalobashi..!!";
+const search = "valo";
+const userInput = "BlackPink"
+const savedUserName = "blackPink"
+
+if (userInput.toLocaleLowerCase() == savedUserName.toLocaleLowerCase()) {
+   console.log("User Exist.");
+}
