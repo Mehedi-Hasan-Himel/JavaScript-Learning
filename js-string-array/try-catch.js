d0cf5a775ceaf8519b5ac406a66e1cb84863b3c9
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-07-01 14:56:50 +0600

    diffrent error

---
+++ js-string-array/try-catch.js
@@ -0,0 +1,19 @@
+const myName = "ami";
+const student = undefined;
+
+name = "tumi";
+
+try {
+  myName = "tumi";
+} catch (error) {
+  console.log("getting error", error);
+}
+
+console.log(myName);
+console.log(100);
+// for (let i = 0; i < 10 i++){
+
+// }
+// name = "tumi"
+
+// console.log(name);
