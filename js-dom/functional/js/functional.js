5ed152b25240b561abd59b38f878f87d99207c53
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-07-06 11:58:05 +0600

    Share function for deposit and withdraw and balance

--- js-dom/functional/js/functional.js
+++ js-dom/functional/js/functional.js
@@ -3,18 +3,42 @@ function getInputValue(fieldId) {
   const valueInText = inputField.value;
   const value = parseFloat(valueInText);
   inputField.value = " ";
-  //   return value;
+  return value;
 }
-function updateTotal(fieldId,amount) {
+function updateTotal(fieldId, amount) {
+  console.log(fieldId, amount);
   const totalTag = document.getElementById(fieldId);
   const previousTotalInText = totalTag.innerText;
   const previousTotal = parseFloat(previousTotalInText);
   const newTotal = previousTotal + amount;
   totalTag.innerText = newTotal;
 }
+function updateBalance(amount, isAdding) {
+  const balanceTag = document.getElementById("balance-total");
+  const balanceInText = balanceTag.innerText;
+  const previousBalance = parseFloat(balanceInText);
+  let newBalance;
+  if (isAdding == true) {
+    newBalance = previousBalance + amount;
+  } else {
+    newBalance = previousBalance - amount;
+  }
+  balanceTag.innerText = newBalance;
+}
 document
   .getElementById("deposit-button")
   .addEventListener("click", function () {
     const amount = getInputValue("deposit-input");
-    updateTotal("deposit-total");
+    console.log(amount);
+    updateTotal("deposit-total", amount);
+    updateBalance(amount, true);
+  });
+
+//   handel withdraw
+document
+  .getElementById("withdraw-button")
+  .addEventListener("click", function () {
+    const amount = getInputValue("withdraw-input");
+    updateTotal("withdraw-total", amount);
+    updateBalance(amount, false);
   });
