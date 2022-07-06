05fc069e467bdeb0559c0cb22d59b5bb21a9c523
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-07-07 00:59:02 +0600

    Revisit id, class selector

--- js-dom/functional/js/functional.js
+++ js-dom/functional/js/functional.js
@@ -5,14 +5,22 @@ function getInputValue(fieldId) {
   inputField.value = " ";
   return value;
 }
+
+function getInnerTextValue(fieldId) {
+  const fieldTag = document.getElementById(fieldId);
+  const fieldValueInText = fieldTag.innerText;
+  const value = parseFloat(fieldValueInText);
+  return value;
+}
+
 function updateTotal(fieldId, amount) {
-  console.log(fieldId, amount);
   const totalTag = document.getElementById(fieldId);
   const previousTotalInText = totalTag.innerText;
   const previousTotal = parseFloat(previousTotalInText);
   const newTotal = previousTotal + amount;
   totalTag.innerText = newTotal;
 }
+
 function updateBalance(amount, isAdding) {
   const balanceTag = document.getElementById("balance-total");
   const balanceInText = balanceTag.innerText;
@@ -25,20 +33,25 @@ function updateBalance(amount, isAdding) {
   }
   balanceTag.innerText = newBalance;
 }
+
 document
   .getElementById("deposit-button")
   .addEventListener("click", function () {
     const amount = getInputValue("deposit-input");
-    console.log(amount);
-    updateTotal("deposit-total", amount);
-    updateBalance(amount, true);
+    if (amount > 0) {
+      updateTotal("deposit-total", amount);
+      updateBalance(amount, true);
+    }
   });
 
-//   handel withdraw
+// handel withdraw
 document
   .getElementById("withdraw-button")
   .addEventListener("click", function () {
     const amount = getInputValue("withdraw-input");
-    updateTotal("withdraw-total", amount);
-    updateBalance(amount, false);
+    const balance = getInnerTextValue("balance-total")
+    if (amount > 0 && amount <= balance) {
+      updateTotal("withdraw-total", amount);
+      updateBalance(amount, false);
+    }
   });
---
+++ js-dom/functional/js/players.js
@@ -0,0 +1,11 @@
+const players = document.getElementsByClassName("player");
+for (const player of players) {
+  player.style.border = "1px solid red";
+  player.style.borderRadius = "10px";
+  player.style.margin = "10px";
+  player.style.padding = "10px";
+}
+
+function addPlayer() {
+   const playersContainer = document.getElementById("players");
+}
