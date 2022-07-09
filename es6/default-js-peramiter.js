4656eec5d9ba405eb77a3e3d18e95a0740deff5e
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-07-09 12:54:15 +0600

    name

--- es6/default-js-peramiter.js
+++ es6/default-js-peramiter.js
@@ -1,16 +1,23 @@
-function add(num1, num2=0) {
+function add(num1, num2 = 0) {
+  // Option - 02
+  // num2 == num2 || 0;
 
-   // Option - 02
-   // num2 == num2 || 0;
-
-   // Option-01
+  // Option-01
   //   if (num2 == undefined) {
   //     num2 = 0;
   //   }
-   
+
   const total = num1 + num2;
   return total;
 }
 
-const result = add(15,10);
+const result = add(15, 10);
 console.log(result);
+
+function fullName(firstName, lastName="Chowdhury") {
+  const name = firstName + " " + lastName;
+  return name;
+}
+
+const myName = fullName("Himel","Hasan")
+console.log(myName);
