2cbcef5c17ce8235ed83165192dd20a462e4732c
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-07-09 12:12:26 +0600

    dom

---
+++ es6/dom.js
@@ -0,0 +1,24 @@
+// Border Start
+document.getElementById("add-border").addEventListener("click", function () {
+  const container = document.getElementById("friend-container");
+  container.style.border = "3px solid yellow ";
+});
+// Border End
+
+function addBackgroundColor() {
+  const friends = document.getElementsByClassName("friend");
+  for (const friend of friends) {
+    friend.style.backgroundColor = "lightblue";
+  }
+}
+
+document.getElementById("add-friend").addEventListener("click", function () {
+  const container = document.getElementById("friend-container");
+  const friendDiv = document.createElement("div");
+  friendDiv.classList.add("friend");
+  friendDiv.innerHTML = `
+    <h3 class="friend-name">New Friend</h3>
+         <p>Dolorem, qui soluta! Unde, vitae?</p>
+    `;
+  container.appendChild(friendDiv);
+});
