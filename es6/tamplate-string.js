4e402f73b71109c92c5f45a84c4eb3113829c8fb
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-07-09 14:59:14 +0600

    Template string, multiple line string, dynamic string

---
+++ es6/tamplate-string.js
@@ -0,0 +1,23 @@
+const priya = "Aisf Akbor";
+const meye = "Meye tumiki dukkkho ceno? ";
+const kobita = `Kobita tumi sopno charini`;
+const multiLine =
+  "This is my first line " +
+  "this is my second line" +
+  "Third line here" +
+  "4th line text here";
+
+const friends = ["Abul", "Babul", "Kabul", "Sabul"];
+const count = 5;
+const old = '<h3 class="friend-name">Friend-4</h3>';
+const old2 = '<h3 class="friend-name">Friend-' + count + "<h3>";
+const new1 = `<h3 class="friend-name">Friend ${friends.length}</h3>
+`;
+
+const first = `Mamun`;
+const second = `Choudhory`;
+const fullName = "This person name is " + first + " " + second;
+const fullNew = `This person name is: ${first} ${second}. His money ${
+  friends.length * 500
+}`;
+console.log(fullNew);
