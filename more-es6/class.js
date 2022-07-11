2e01076978bf7eb8714d266b623738bb46ffdc32
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-07-11 11:51:50 +0600

    Class, constructor, method, create object from class

--- more-es6/class.js
+++ more-es6/class.js
@@ -2,19 +2,17 @@ class Support {
   name;
   roll = `Support Web Dev`;
   address = `BD`;
-  constructor(name,address) {
-     this.name = name;
-     this.address = address;
+  constructor(name, address) {
+    this.name = name;
+    this.address = address;
   }
   startSession() {
-    console.log(`Start a support session. `);
+    console.log(this.name, `Start a support session. `);
   }
 }
 
 const amir = new Support(`Amir Khan`, `India`);
-const salman = new Support(`Salman Khan`,`Dubai`);
-console.log(amir);
-console.log(salman);
+const salman = new Support(`Salman Khan`, `Dubai`);
+const srk = new Support(`SRK Khan`, `Nepal `);
+console.log(amir, salman, srk);
 
-
-amir.startSession
