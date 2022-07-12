a08bb31a16657ab2011caf348c28cd9734fd31b9
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-07-12 22:55:09 +0600

    Load More Data, More API, Send Data To Function

--- api-intro/js/api.js
+++ api-intro/js/api.js
@@ -3,3 +3,19 @@ function loadData() {
     .then((response) => response.json())
     .then((data) => console.log(data));
 }
+
+function loadUsers() {
+  fetch(`https://jsonplaceholder.typicode.com/users`)
+    .then((res) => res.json())
+    .then((data) => displayUsers(data));
+}
+
+function loadPost() {
+  fetch(`https://jsonplaceholder.typicode.com/posts`)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
+
+function displayUsers(data) {
+  console.log(data);
+}
