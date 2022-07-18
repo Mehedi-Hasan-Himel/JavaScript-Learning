b0ea142cf507a46f2c4cf10a3d960cba57517357
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-07-18 12:56:44 +0600

    comparison

---
+++ js-concep/doubble-tripple.js
@@ -0,0 +1,20 @@
+const first = 1;
+const second = true;
+if (first == second) {
+//   console.log(`Condition is true`);
+} else {
+//   console.log(`Condition is false`);
+}
+
+// more comparison
+// const a = { name: `Ali` };
+// const b = { name: `Ali` };
+
+const a = []
+const b = []
+
+if (a === b) {
+  console.log(`Both are same.`);
+} else {
+  console.log(`they aren't same.`);
+}
