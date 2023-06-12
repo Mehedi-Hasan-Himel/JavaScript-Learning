eb3024773ab0ec901e8efac52f777fe5cff66791
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-06-12 12:01:52 +0600

    oop random objects

---
+++ OOP/random-objects.js
@@ -0,0 +1,75 @@
+// Object of Father
+const father = {
+  name: "Mustafa Kamal",
+  age: 52,
+  profession: "Employee",
+  hobbies: ["Keeping Pets", "Cooking"],
+  address: {
+    thana: "Sreepur",
+    district: "Gazipur",
+    country: "Bangladesh",
+  },
+};
+
+// Object of Mother
+const mother = {
+  name: "Helena",
+  age: 30,
+  profession: "Housewife",
+  hobbies: ["Cooking", "Gardening"],
+  address: {
+    thana: "Sreepur",
+    district: "Gazipur",
+    country: "Bangladesh",
+  },
+};
+
+// Object of Brother
+const brother = {
+  name: "Hira",
+  age: 15,
+  profession: "Student",
+  hobbies: ["Playing Cricket", "Fishing"],
+  address: {
+    thana: "Sreepur",
+    district: "Gazipur",
+    country: "Bangladesh",
+  },
+};
+
+// Object of Phone
+const phone = {
+  brand: "oppo",
+  price: 5000,
+  color: "silver",
+  storage: "32GB",
+  camera: "16MP",
+  display: "5.5 inch",
+  ram: "4GB",
+};
+
+// object of Laptop
+const laptop = {
+   brand: "Apple",
+   parice: 80000,
+   color: "silver",
+   storage: 128,
+   ram: "8GB",
+   processor: "Intel Core i5",
+   display: "13 inch",
+   graphics: "Intel Iris Plus Graphics 640",
+   battery: "Up to 10 hours",
+   weight: "1.37 kg",
+   warranty: "1 year",
+}
+
+// object of pet
+const pet = {
+   name: "Mini",
+   age: "4 months",
+   color: "Black",
+   breed: "Cat",
+}
+
+
+
